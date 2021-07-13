26fb9ebe295695e8e26ba1a9cb5501d44dec4cbd
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2021-07-13 14:43:21 +0100

    Update backendconfig.py

--- backendconfig.py
+++ backendconfig.py
@@ -3,7 +3,7 @@ import preprocessing
 
 db = {
     "user": "oiguser",
-    "password": "nightshade900",
+    "password": "password",
     "host": "127.0.0.1",
     "port": "5432",
     "database": "oig"
